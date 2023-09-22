625eeea24fd11fa0045491bd252aa9625031ad67
TanLiangJin <liangjin0228@gmail.com>
2023-09-22 23:27:53 +0800

    init with header & footer

---
+++ src/layouts/default/Default.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <default-bar />
+
+    <default-view />
+    <MyFooter />
+  </v-app>
+</template>
+
+<script setup>
+import DefaultBar from './AppBar.vue'
+import DefaultView from './View.vue'
+import MyFooter from '../../components/MyFooter.vue'
+</script>
