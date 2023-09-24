258aa60a38cc1155b2ccfa52ad0b654893812771
TanLiangJin <liangjin0228@gmail.com>
2023-09-24 22:37:27 +0800

    contact頁面排版

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,18 @@
 <template>
-    <v-form>
-        <v-autocomplete v-model="selected" :items="items" chips hide-details hide-no-data hide-selected label="To" multiple
-            single-line></v-autocomplete>
+    <v-container>
+        <v-form>
+            <v-autocomplete v-model="selected" :items="items" chips hide-details hide-no-data hide-selected label="To"
+                multiple single-line></v-autocomplete>
 
-        <v-divider></v-divider>
+            <v-divider></v-divider>
 
-        <v-text-field v-model="subject" hide-details label="Subject" single-line></v-text-field>
+            <v-text-field v-model="subject" hide-details label="Subject" single-line></v-text-field>
 
-        <v-divider></v-divider>
+            <v-divider></v-divider>
 
-        <v-textarea v-model="title" counter label="Message" maxlength="120" single-line></v-textarea>
-    </v-form>
+            <v-textarea v-model="title" counter label="Message" maxlength="120" single-line></v-textarea>
+        </v-form>
+    </v-container>
 </template>
 <script>
 export default {
