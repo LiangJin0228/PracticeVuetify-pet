ede40e0e0d0d3dc805507ae357b90db98404940e
TanLiangJin <liangjin0228@gmail.com>
2023-09-28 23:09:26 +0800

    vite fixed base url incorrect

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "PracticeVuetify-pet/",
+  base: "/PracticeVuetify-pet",
   plugins: [
     vue({
       template: { transformAssetUrls },
